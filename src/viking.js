3b5a5562e1a233e69b1422666e5f339b2f5b39ea
AitorMP <aitor.martinez.pons@gmail.com>
2022-02-09 20:52:33 +0000

    finished all

--- src/viking.js
+++ src/viking.js
@@ -15,7 +15,7 @@ class Soldier {
 // Viking
 class Viking extends Soldier {
   constructor(name, health, strength) {
-    super();
+    super(health, strength);
     this.name = name;
     this.health = health;
     this.strength = strength;
@@ -35,10 +35,67 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+  vikingAttack() {
+    let randomViking =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    let randomSaxon =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let damageViking = randomViking.strength;
+    let damage = randomSaxon.receiveDamage(damageViking);
+    if (randomSaxon.health <= 0) {
+      this.saxonArmy = this.saxonArmy.filter((saxon) => saxon !== randomSaxon);
+    }
+    return damage;
+  }
+  saxonAttack() {
+    let randomViking =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    let randomSaxon =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let damageSaxon = randomSaxon.strength;
+
+    let damage = randomViking.receiveDamage(damageSaxon);
+    if (randomViking.health <= 0) {
+      this.vikingArmy = this.vikingArmy.filter(
+        (viking) => viking !== randomViking
+      );
+    }
+    return damage;
+  }
+  showStatus() {
+    if (this.saxonArmy.length === 0) {
+      return 'Vikings have won the war of the century!';
+    } else if (this.vikingArmy.length === 0) {
+      return 'Saxons have fought for their lives and survived another day...';
+    } else {
+      return 'Vikings and Saxons are still in the thick of battle.';
+    }
+  }
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
